3914d9c2021d6e50ce00e3fabcd7a30cedaad285
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2021-09-18 14:55:15 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -16,7 +16,9 @@ const solcNightly = {
 };
 
 const useSolcNightly = process.env.SOLC_NIGHTLY === 'true';
-
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 module.exports = {
   networks: {
     development: {
@@ -31,9 +33,22 @@ module.exports = {
       gas: 0xfffffffffff,
       gasPrice: 0x01,
     },
+    bsc: {
+      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
   compilers: {
     solc: useSolcNightly ? solcNightly : solcStable,
   },
+  // Configure your compilers
+//   compilers: {
+//     solc: {
+//       version: "^0.6.12", // A version or constraint - Ex. "^0.5.0"
+//     }
+//   },
   plugins: ['solidity-coverage'],
 };
